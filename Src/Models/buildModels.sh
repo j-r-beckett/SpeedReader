c1e3c215a4f61c333e9b70fc602be8a1aa9571d3
Jimmy Beckett <jimmybeckett1+github@gmail.com>
2025-05-27 17:17:54 -0700

    Fix model generation

--- Src/Models/buildModels.sh
+++ Src/Models/buildModels.sh
@@ -1,12 +1,10 @@
-OUTPUT_DIR="${1:-models}"
-echo "OUTPUT_DIR received: '$OUTPUT_DIR'"
 echo "Building models..."
 
 docker build -t modelbuilder:latest --progress plain .
 CONTAINER_ID=$(docker create modelbuilder:latest)
 docker start "$CONTAINER_ID" > /dev/null
 
-mkdir -p "$OUTPUT_DIR"
-docker cp "$CONTAINER_ID":/models/. "$OUTPUT_DIR"/
+mkdir -p models
+docker cp "$CONTAINER_ID":/models/. models/
 
 docker rm "$CONTAINER_ID" > /dev/null
