7f63741524ae3e68894a55f6db53182f301f8b2d
Jimmy Beckett <jimmybeckett1+github@gmail.com>
2025-06-08 23:02:34 -0700

    Fix concurrency bug in ONNX session initialization

--- Src/Models/ModelZoo.cs
+++ Src/Models/ModelZoo.cs
@@ -5,21 +5,26 @@ namespace Models;
 
 public class ModelZoo
 {
+    private static readonly Lock s_lock = new();
+
     public static InferenceSession GetInferenceSession(Model model) => GetInferenceSession(model, new SessionOptions());
 
     public static InferenceSession GetInferenceSession(Model model, SessionOptions options)
     {
-        string resourceName = GetResourceName(model);
-        var assembly = Assembly.GetExecutingAssembly();
-        
-        using var stream = assembly.GetManifestResourceStream(resourceName);
-        if (stream == null)
-            throw new FileNotFoundException($"Embedded resource '{resourceName}' not found");
-            
-        var modelBytes = new byte[stream.Length];
-        stream.ReadExactly(modelBytes);
-        
-        return new InferenceSession(modelBytes, options);
+        lock (s_lock)
+        {
+            string resourceName = GetResourceName(model);
+            var assembly = Assembly.GetExecutingAssembly();
+
+            using var stream = assembly.GetManifestResourceStream(resourceName);
+            if (stream == null)
+                throw new FileNotFoundException($"Embedded resource '{resourceName}' not found");
+
+            var modelBytes = new byte[stream.Length];
+            stream.ReadExactly(modelBytes);
+
+            return new InferenceSession(modelBytes, options);
+        }
     }
 
     private static string GetResourceName(Model model)
@@ -30,7 +35,7 @@ public class ModelZoo
             Model.SVTRv2 => "svtrv2_base_ctc",
             _ => throw new ArgumentException($"Unknown model {model}")
         };
-        
+
         return $"Models.models.{modelName}.end2end.onnx";
     }
 }
