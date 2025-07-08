796f1563c81b75a6ed7b3b5a5ee99dcce2497bef
Jimmy Beckett <jimmybeckett1+github@gmail.com>
2025-07-07 23:38:30 -0700

    Use static model size

--- Src/Ocr/Blocks/SVTRBlock.cs
+++ Src/Ocr/Blocks/SVTRBlock.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks.Dataflow;
+using System.Numerics.Tensors;
 using Microsoft.ML.OnnxRuntime;
 using Ocr.Visualization;
 using SixLabors.ImageSharp;
@@ -8,70 +9,51 @@ namespace Ocr.Blocks;
 
 public static class SVTRBlock
 {
-    public static IPropagatorBlock<(Image<Rgb24>, List<Rectangle>, VizBuilder), (Image<Rgb24>, List<Rectangle>, List<string>, VizBuilder)> Create(InferenceSession session)
+    public static IPropagatorBlock<(List<Rectangle>, Image<Rgb24>, VizBuilder), (Image<Rgb24>, List<Rectangle>, List<string>, VizBuilder)> Create(InferenceSession session)
     {
-        var batchBlock = CreateAdaptiveBatchBlock<(Image<Rgb24>, List<Rectangle>, VizBuilder)>();
         var preProcessingBlock = CreatePreProcessingBlock();
         var modelRunnerBlock = CreateModelRunnerBlock(session);
         var postProcessingBlock = CreatePostProcessingBlock();
 
-        batchBlock.LinkTo(preProcessingBlock, new DataflowLinkOptions { PropagateCompletion = true });
         preProcessingBlock.LinkTo(modelRunnerBlock, new DataflowLinkOptions { PropagateCompletion = true });
         modelRunnerBlock.LinkTo(postProcessingBlock, new DataflowLinkOptions { PropagateCompletion = true });
 
-        return DataflowBlock.Encapsulate(batchBlock, postProcessingBlock);
+        return DataflowBlock.Encapsulate(preProcessingBlock, postProcessingBlock);
     }
 
-    private static TransformBlock<T, T[]> CreateAdaptiveBatchBlock<T>()
+
+    private static TransformBlock<(List<Rectangle>, Image<Rgb24>, VizBuilder), (float[], List<Rectangle>, Image<Rgb24>, VizBuilder)> CreatePreProcessingBlock()
     {
-        return new TransformBlock<T, T[]>(data => [data]);
+        return new TransformBlock<(List<Rectangle> Rectangles, Image<Rgb24> Image, VizBuilder VizBuilder), (float[], List<Rectangle>, Image<Rgb24>, VizBuilder)>(input
+            => (SVTRv2.PreProcessSingle(input.Image, input.Rectangles), input.Rectangles, input.Image, input.VizBuilder));
     }
 
-    private static TransformBlock<(Image<Rgb24>, List<Rectangle>, VizBuilder)[], (Buffer<float>, (Image<Rgb24>, List<Rectangle>, VizBuilder)[])> CreatePreProcessingBlock()
+    private static TransformBlock<(float[], List<Rectangle>, Image<Rgb24>, VizBuilder), (float[], List<Rectangle>, Image<Rgb24>, VizBuilder)> CreateModelRunnerBlock(InferenceSession session)
     {
-        return new TransformBlock<(Image<Rgb24>, List<Rectangle>, VizBuilder)[], (Buffer<float>, (Image<Rgb24>, List<Rectangle>, VizBuilder)[])>(batch =>
+        return new TransformBlock<(float[] ProcessedRegions, List<Rectangle> Rectangles, Image<Rgb24> OriginalImage, VizBuilder VizBuilder), (float[], List<Rectangle>, Image<Rgb24>, VizBuilder)>(input =>
         {
-            var images = batch.Select(item => item.Item1).ToArray();
-            var rectangles = batch.Select(item => item.Item2).ToArray();
+            // Model input should be [num_rectangles, 3, 48, 320] - rectangles, 3 channels, height 48, width 320
+            int numRectangles = input.Rectangles.Count;
+            var inputTensor = Tensor.Create(input.ProcessedRegions, [ numRectangles, 3, 48, 320 ]);
 
-            var buffer = SVTRv2.PreProcess(images, rectangles);
-            return (buffer, batch);
+            var outputBuffer = ModelRunner.Run(session, inputTensor);
+
+            float[] outputData = outputBuffer.AsSpan().ToArray();
+            outputBuffer.Dispose();
+
+            return (outputData, input.Rectangles, input.OriginalImage, input.VizBuilder);
         });
     }
 
-    private static TransformBlock<(Buffer<float>, (Image<Rgb24>, List<Rectangle>, VizBuilder)[]), (Buffer<float>, (Image<Rgb24>, List<Rectangle>, VizBuilder)[])> CreateModelRunnerBlock(InferenceSession session)
+    private static TransformBlock<(float[], List<Rectangle>, Image<Rgb24>, VizBuilder), (Image<Rgb24>, List<Rectangle>, List<string>, VizBuilder)> CreatePostProcessingBlock()
     {
-        return new TransformBlock<(Buffer<float> Buffer, (Image<Rgb24>, List<Rectangle>, VizBuilder)[] Batch), (Buffer<float>, (Image<Rgb24>, List<Rectangle>, VizBuilder)[])>(data =>
+        return new TransformBlock<(float[] RawResult, List<Rectangle> Rectangles, Image<Rgb24> OriginalImage, VizBuilder VizBuilder), (Image<Rgb24>, List<Rectangle>, List<string>, VizBuilder)>(input =>
         {
-            var result = ModelRunner.Run(session, data.Buffer.AsTensor());
-            data.Buffer.Dispose();
-            return (result, data.Batch);
-        });
-    }
-
-    private static TransformManyBlock<(Buffer<float>, (Image<Rgb24>, List<Rectangle>, VizBuilder)[]), (Image<Rgb24>, List<Rectangle>, List<string>, VizBuilder)> CreatePostProcessingBlock()
-    {
-        return new TransformManyBlock<(Buffer<float> Buffer, (Image<Rgb24>, List<Rectangle>, VizBuilder)[] Batch), (Image<Rgb24>, List<Rectangle>, List<string>, VizBuilder)>(data =>
-        {
-            var recognizedTexts = SVTRv2.PostProcess(data.Buffer);
-            data.Buffer.Dispose();
-
-            // Return tuple combining original images, rectangles, recognized texts, and VizBuilder
-            var results = new List<(Image<Rgb24>, List<Rectangle>, List<string>, VizBuilder)>();
-            int textIndex = 0;
-
-            foreach (var (image, rectangles, vizBuilder) in data.Batch)
-            {
-                var batchTexts = new List<string>();
-                for (int i = 0; i < rectangles.Count; i++)
-                {
-                    batchTexts.Add(recognizedTexts[textIndex++]);
-                }
-                vizBuilder.AddRecognitionResults(batchTexts);
-                results.Add((image, rectangles, batchTexts, vizBuilder));
-            }
-
-            return results;
+            var recognizedTexts = SVTRv2.PostProcessSingle(input.RawResult, input.Rectangles.Count);
+            
+            input.VizBuilder.AddRecognitionResults(recognizedTexts.ToList());
+            
+            return (input.OriginalImage, input.Rectangles, recognizedTexts.ToList(), input.VizBuilder);
         });
     }
 }
