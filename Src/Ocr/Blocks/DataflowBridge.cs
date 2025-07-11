eab319dfd9ccd39d078fa92f922f60755229de2e
Jimmy Beckett <jimmybeckett1+github@gmail.com>
2025-07-10 23:46:55 -0700

    Use DataflowBridge

--- Src/Ocr/Blocks/DataflowBridge.cs
+++ Src/Ocr/Blocks/DataflowBridge.cs
@@ -7,17 +7,17 @@ public class DataflowBridge<TIn, TOut> : IAsyncDisposable
     private readonly TransformBlock<(TIn Input, TaskCompletionSource<TOut> Tcs), TIn> _origin;
     private readonly ActionBlock<Tuple<TOut, TaskCompletionSource<TOut>>> _terminus;
 
-    // Block MUST maintain an ordered 1-1 correspondence between inputs and outputs
-    public DataflowBridge(IPropagatorBlock<TIn, TOut> primary)
+    // Transformer block MUST maintain an ordered 1-1 correspondence between inputs and outputs
+    public DataflowBridge(IPropagatorBlock<TIn, TOut> transformer)
     {
-        var secondary = new BufferBlock<TaskCompletionSource<TOut>>();
+        var completionSources = new BufferBlock<TaskCompletionSource<TOut>>();
 
         _origin = new TransformBlock<(TIn Input, TaskCompletionSource<TOut> Tcs), TIn>(item =>
         {
-            if (!secondary.Post(item.Tcs))
+            if (!completionSources.Post(item.Tcs))
             {
                 // secondary is unbounded, if this happens something has gone wrong
-                throw new InvalidOperationException($"{nameof(secondary)} declined input");
+                throw new InvalidOperationException($"{nameof(completionSources)} declined input");
             }
 
             return item.Input;
@@ -26,7 +26,7 @@ public class DataflowBridge<TIn, TOut> : IAsyncDisposable
             BoundedCapacity = Environment.ProcessorCount
         });
 
-        _origin.LinkTo(primary, new DataflowLinkOptions
+        _origin.LinkTo(transformer, new DataflowLinkOptions
         {
             PropagateCompletion = true
         });
@@ -36,8 +36,8 @@ public class DataflowBridge<TIn, TOut> : IAsyncDisposable
             Greedy = false
         });
 
-        primary.LinkTo(joiner.Target1);
-        secondary.LinkTo(joiner.Target2);
+        transformer.LinkTo(joiner.Target1);
+        completionSources.LinkTo(joiner.Target2);
 
         _terminus = new ActionBlock<Tuple<TOut, TaskCompletionSource<TOut>>>(pair => pair.Item2.SetResult(pair.Item1));
 
@@ -46,16 +46,16 @@ public class DataflowBridge<TIn, TOut> : IAsyncDisposable
             PropagateCompletion = true
         });
 
-        primary.Completion.ContinueWith(primaryCompletionTask =>
+        transformer.Completion.ContinueWith(primaryCompletionTask =>
         {
             if (primaryCompletionTask.IsFaulted)
             {
-                var orphanedCompletionHandler = new ActionBlock<TaskCompletionSource<TOut>>(orphanedCompletion => 
+                var orphanedCompletionHandler = new ActionBlock<TaskCompletionSource<TOut>>(orphanedCompletion =>
                     orphanedCompletion.SetException(new DataflowBridgeException("Dataflow mesh completed unexpectedly", primaryCompletionTask.Exception)));
-                secondary.LinkTo(orphanedCompletionHandler, new DataflowLinkOptions { PropagateCompletion = true });
+                completionSources.LinkTo(orphanedCompletionHandler, new DataflowLinkOptions { PropagateCompletion = true });
             }
 
-            secondary.Complete();
+            completionSources.Complete();
             joiner.Complete();
         });
     }
