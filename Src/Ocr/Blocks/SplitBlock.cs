e3554c1fa70fd0a055498cb46cfbebbcb68c45ea
Jimmy Beckett <jimmybeckett1+github@gmail.com>
2025-07-17 23:06:01 -0700

    MergeBlock and SplitBlock

---
+++ Src/Ocr/Blocks/SplitBlock.cs
@@ -0,0 +1,39 @@
+using System.Threading.Tasks.Dataflow;
+
+namespace Ocr.Blocks;
+
+public class SplitBlock<TIn, TOutLeft, TOutRight>
+{
+    public readonly ITargetBlock<TIn> Target;
+    public readonly ISourceBlock<TOutLeft> LeftSource;
+    public readonly ISourceBlock<TOutRight> RightSource;
+
+    public SplitBlock(Func<TIn, (TOutLeft, TOutRight)> splitter)
+    {
+        var leftSource = new BufferBlock<TOutLeft>();
+        var rightSource = new BufferBlock<TOutRight>();
+
+        Target = new ActionBlock<TIn>(async item =>
+        {
+            (TOutLeft left, TOutRight right) = splitter(item);
+            await Task.WhenAll(leftSource.SendAsync(left), rightSource.SendAsync(right));
+        });
+
+        Target.Completion.ContinueWith(t =>
+        {
+            if (t.IsFaulted)
+            {
+                ((IDataflowBlock)leftSource).Fault(t.Exception);
+                ((IDataflowBlock)rightSource).Fault(t.Exception);
+            }
+            else
+            {
+                leftSource.Complete();
+                rightSource.Complete();
+            }
+        });
+
+        LeftSource = leftSource;
+        RightSource = rightSource;
+    }
+}
