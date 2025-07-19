14868a31dc503ac4905ae7a89c539c5cb949e9c1
Jimmy Beckett <jimmybeckett1+github@gmail.com>
2025-07-18 20:52:54 -0700

    Use 640x640 for DBNet inference

--- Src/Ocr/SVTRv2.cs
+++ Src/Ocr/SVTRv2.cs
@@ -6,27 +6,36 @@ using SixLabors.ImageSharp.Processing;
 
 namespace Ocr;
 
-public static class SVTRv2
+public class SVTRv2
 {
-    private const int ModelHeight = 48;
-    private const int ModelWidth = 320;
+    private readonly int _width;
+    private readonly int _height;
 
-    public static float[] PreProcess(Image<Rgb24> image, TextBoundary textBoundary)
+    public SVTRv2(SvtrConfiguration config)
     {
-        float[] data = new float[ModelHeight * ModelWidth * 3];
+        _width = config.Width;
+        _height = config.Height;
+    }
+
+    public int Width => _width;
+    public int Height => _height;
+
+    public float[] PreProcess(Image<Rgb24> image, TextBoundary textBoundary)
+    {
+        float[] data = new float[_height * _width * 3];
 
         using var croppedImage = image.Clone(x => x.Crop(textBoundary.AARectangle));
 
-        Resampling.AspectResizeInto(croppedImage, data, ModelWidth, ModelHeight, 127.5f);
+        Resampling.AspectResizeInto(croppedImage, data, _width, _height, 127.5f);
 
         // Convert to CHW format in place
-        TensorOps.NhwcToNchw(data, [ModelHeight, ModelWidth, 3]);
+        TensorOps.NhwcToNchw(data, [_height, _width, 3]);
 
         // Apply SVTRv2 normalization: [0,255] -> [-1,1] for each channel
         for (int channel = 0; channel < 3; channel++)
         {
-            int channelOffset = channel * ModelHeight * ModelWidth;
-            var channelTensor = Tensor.Create(data, channelOffset, [ModelHeight, ModelWidth], default);
+            int channelOffset = channel * _height * _width;
+            var channelTensor = Tensor.Create(data, channelOffset, [_height, _width], default);
 
             Tensor.Divide(channelTensor, 127.5f, channelTensor);
             Tensor.Subtract(channelTensor, 1.0f, channelTensor);
@@ -36,7 +45,7 @@ public static class SVTRv2
     }
 
 
-    public static (string text, double confidence) PostProcess(float[] modelOutput)
+    public (string text, double confidence) PostProcess(float[] modelOutput)
     {
         return CTC.DecodeSingleSequence(modelOutput, CharacterDictionary.Count);
     }
