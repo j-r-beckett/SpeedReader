84e7e6cd701ce1e8dba1d8e809b6d9c248c149f3
Jimmy Beckett <jimmybeckett1+github@gmail.com>
2025-07-22 22:54:06 -0700

    Add control plane benchmarking

--- Src/Ocr/Blocks/DBNetBlock.cs
+++ Src/Ocr/Blocks/DBNetBlock.cs
@@ -11,11 +11,11 @@ public class DBNetBlock
 {
     public IPropagatorBlock<(Image<Rgb24>, VizBuilder), (List<TextBoundary>, Image<Rgb24>, VizBuilder)> Target { get; }
 
-    public DBNetBlock(InferenceSession session, DbNetConfiguration config, Meter meter)
+    public DBNetBlock(InferenceSession session, OcrConfiguration config, Meter meter)
     {
-        var preprocessingBlock = new DBNetPreprocessingBlock(config);
+        var preprocessingBlock = new DBNetPreprocessingBlock(config.DbNet);
         var modelRunnerBlock = new DBNetModelRunnerBlock(session, config, meter);
-        var postprocessingBlock = new DBNetPostprocessingBlock(config);
+        var postprocessingBlock = new DBNetPostprocessingBlock(config.DbNet);
 
         preprocessingBlock.Target.LinkTo(modelRunnerBlock.Target, new DataflowLinkOptions { PropagateCompletion = true });
         modelRunnerBlock.Target.LinkTo(postprocessingBlock.Target, new DataflowLinkOptions { PropagateCompletion = true });
--- Src/Ocr/Blocks/SVTRModelRunnerBlock.cs
+++ Src/Ocr/Blocks/SVTRModelRunnerBlock.cs
@@ -11,12 +11,12 @@ public class SVTRModelRunnerBlock
 {
     public IPropagatorBlock<(float[], TextBoundary, Image<Rgb24>, VizBuilder), (float[], TextBoundary, Image<Rgb24>, VizBuilder)> Target { get; }
 
-    public SVTRModelRunnerBlock(InferenceSession session, SvtrConfiguration config, Meter meter)
+    public SVTRModelRunnerBlock(InferenceSession session, OcrConfiguration config, Meter meter)
     {
         var splitBlock = new SplitBlock<(float[], TextBoundary, Image<Rgb24>, VizBuilder), float[], (TextBoundary, Image<Rgb24>, VizBuilder)>(
             input => (input.Item1, (input.Item2, input.Item3, input.Item4)));
 
-        var inferenceBlock = new InferenceBlock(session, [3, config.Height, config.Width], meter, "svtr");
+        var inferenceBlock = new InferenceBlock(session, [3, config.Svtr.Height, config.Svtr.Width], meter, "svtr", config.CacheFirstInference);
 
         var mergeBlock = new MergeBlock<float[], (TextBoundary, Image<Rgb24>, VizBuilder), (float[], TextBoundary, Image<Rgb24>, VizBuilder)>(
             (result, passthrough) => (result, passthrough.Item1, passthrough.Item2, passthrough.Item3));
