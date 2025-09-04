cb94f167d670b713a64328fcbacb2db03890a82c
Jimmy Beckett <jimmybeckett1@gmail.com>
2025-09-03 23:37:03 -0700

    Fix bug limiting max inference parallelism

--- Src/Ocr/Blocks/MergeBlock.cs
+++ Src/Ocr/Blocks/MergeBlock.cs
@@ -10,15 +10,18 @@ public class MergeBlock<TInLeft, TInRight, TOut>
 
     public MergeBlock(Func<TInLeft, TInRight, TOut> merge)
     {
+        var unboundedBuffer = new BufferBlock<TInRight>();
+
         var joinBlock = new JoinBlock<TInLeft, TInRight>(new GroupingDataflowBlockOptions { Greedy = false, BoundedCapacity = 1 });
 
         var source = new TransformBlock<Tuple<TInLeft, TInRight>, TOut>(input => merge(input.Item1, input.Item2),
             new ExecutionDataflowBlockOptions { BoundedCapacity = 1 });
 
+        unboundedBuffer.LinkTo(joinBlock.Target2, new DataflowLinkOptions { PropagateCompletion = true });
         joinBlock.LinkTo(source, new DataflowLinkOptions { PropagateCompletion = true });
 
         LeftTarget = joinBlock.Target1;
-        RightTarget = joinBlock.Target2;
+        RightTarget = unboundedBuffer;
         Source = source;
     }
 }
