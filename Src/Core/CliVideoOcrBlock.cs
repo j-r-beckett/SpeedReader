326304a8504c8cabf3e4f2a9c671bbf235fc44b8
Jimmy Beckett <jimmybeckett1@gmail.com>
2025-09-11 17:13:15 -0700

    Crude, but functional, video ocr

--- Src/Core/CliVideoOcrBlock.cs
+++ Src/Core/CliVideoOcrBlock.cs
@@ -1,7 +1,9 @@
 using System.Diagnostics.Metrics;
+using System.IO.Pipelines;
 using System.Threading.Tasks.Dataflow;
 using Ocr;
 using Ocr.Blocks;
+using Ocr.Visualization;
 using Resources;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
@@ -12,6 +14,7 @@ namespace Core;
 public class CliVideoOcrBlock
 {
     public ISourceBlock<OcrResult> ResultsBlock { get; init; }
+    public Task Completion { get; init; }
 
     public CliVideoOcrBlock(string videoFilePath, int sampleRate)
     {
@@ -20,17 +23,92 @@ public class CliVideoOcrBlock
         var svtrSession = modelProvider.GetSession(Model.SVTRv2);
         var ocrBlock = new OcrBlock(dbnetSession, svtrSession, new OcrConfiguration(), new Meter("SpeedReader.Ocr"));
 
-        using var fileStream = new FileStream(videoFilePath, FileMode.Open, FileAccess.Read);
+        var fileStream = new FileStream(videoFilePath, FileMode.Open, FileAccess.Read);
         var videoOcrBlock = new VideoOcrBlock(ocrBlock, fileStream, sampleRate);
 
-        var splitBlock =
-            new SplitBlock<(Image<Rgb24> Img, OcrResult Result), (Image<Rgb24>, OcrResult), OcrResult>(item =>
+        ITargetBlock<Image<Rgb24>>? ffmpegEncoderBlock = null;
+        PipeReader? encodedOutput = null;
+        Task<Stream>? videoOutputTask = null;
+
+        var counter = 0;
+        var transformer = new TransformBlock<(Image<Rgb24> Img, OcrResult Result, VizBuilder VizBuilder), OcrResult>(async item =>
+        {
+            // Render the visualization with OCR results
+            var visualizedImage = item.VizBuilder.Render();
+
+            if (ffmpegEncoderBlock == null)
             {
-                return ((item.Img, item.Result), item.Result);
-            });
+                ffmpegEncoderBlock = new FfmpegEncoderBlockCreator().CreateFfmpegEncoderBlock(visualizedImage.Width,
+                    visualizedImage.Height, 30, out encodedOutput, CancellationToken.None);
 
-        videoOcrBlock.Source.LinkTo(splitBlock.Target);
+                // Start consuming the encoded output concurrently
+                videoOutputTask = ReadEncodedOutputAsync(encodedOutput);
+            }
 
-        ResultsBlock = splitBlock.RightSource;
+            await visualizedImage.SaveAsPngAsync($"img_{counter++}.png");
+
+            await ffmpegEncoderBlock.SendAsync(visualizedImage);
+
+            return item.Result;
+        });
+
+        videoOcrBlock.Source.LinkTo(transformer, new DataflowLinkOptions { PropagateCompletion = true });
+
+        ResultsBlock = transformer;
+        Completion = transformer.Completion.ContinueWith(async _ =>
+        {
+            if (ffmpegEncoderBlock != null)
+            {
+                ffmpegEncoderBlock.Complete();
+                await ffmpegEncoderBlock.Completion;
+
+                // Wait for video output and save to file
+                if (videoOutputTask != null)
+                {
+                    var videoStream = await videoOutputTask;
+                    await using var outFile = File.Create("video_viz.webm");
+                    videoStream.Position = 0;
+                    await videoStream.CopyToAsync(outFile);
+                    videoStream.Dispose();
+                }
+            }
+
+            fileStream.Dispose();
+            modelProvider.Dispose();
+        }).Unwrap();
+    }
+
+    private static async Task<Stream> ReadEncodedOutputAsync(PipeReader reader)
+    {
+        var outputStream = new MemoryStream();
+
+        try
+        {
+            while (true)
+            {
+                var result = await reader.ReadAsync();
+                var buffer = result.Buffer;
+
+                if (buffer.Length > 0)
+                {
+                    // Copy buffer to memory stream
+                    foreach (var segment in buffer)
+                    {
+                        await outputStream.WriteAsync(segment);
+                    }
+                }
+
+                reader.AdvanceTo(buffer.End);
+
+                if (result.IsCompleted)
+                    break;
+            }
+        }
+        finally
+        {
+            await reader.CompleteAsync();
+        }
+
+        return outputStream;
     }
 }
