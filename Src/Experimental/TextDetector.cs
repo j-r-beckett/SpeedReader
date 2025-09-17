1059a740554cf835d0606b81b7ad0252bbc61614
Jimmy Beckett <jimmybeckett1@gmail.com>
2025-09-17 01:21:15 -0700

    Simplify parallelism model

--- Src/Experimental/TextDetector.cs
+++ Src/Experimental/TextDetector.cs
@@ -1,6 +1,7 @@
 // Copyright (c) 2025 j-r-beckett
 // Licensed under the Apache License, Version 2.0
 
+using Experimental.Inference;
 using Ocr;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
@@ -9,19 +10,19 @@ namespace Experimental;
 
 public class TextDetector
 {
-    private readonly Func<float[], Task<float[]>> _runInference;
+    private readonly ModelRunner _modelRunner;
 
-    public TextDetector(Func<float[], Task<float[]>> runInference) => _runInference = runInference;
+    public TextDetector(ModelRunner modelRunner) => _modelRunner = modelRunner;
 
     public async Task<List<TextBoundary>> Detect(Image<Rgb24> image)
     {
         var modelInput = Preprocess(image);
-        var modelOutput = await _runInference(modelInput);
-        var result = Postprocess(modelOutput);
+        var modelOutput = await _modelRunner.Run(modelInput);
+        var result = Postprocess(modelOutput, image);
         return result;
     }
 
     private float[] Preprocess(Image<Rgb24> image) => [];
 
-    private List<TextBoundary> Postprocess(float[] modelOutput) => [];
+    private List<TextBoundary> Postprocess(float[] modelOutput, Image<Rgb24> image) => [];
 }
