251a47be7f88061da18b7d98092491da2fce022a
Jimmy Beckett <jimmybeckett1+github@gmail.com>
2025-09-18 18:26:29 -0700

    Refactor inference

--- Src/Experimental/Inference/ModelRunner.cs
+++ Src/Experimental/Inference/ModelRunner.cs
@@ -2,68 +2,19 @@
 // Licensed under the Apache License, Version 2.0
 
 using System.Diagnostics;
-using System.Threading.Channels;
 using Microsoft.ML.OnnxRuntime;
 
 namespace Experimental.Inference;
 
 public abstract class ModelRunner : IAsyncDisposable
 {
-    // Subclasses should read inputs from Inputs -> do inference -> write inference results to Outputs
-    protected readonly Channel<(float[] Data, int[] Shape)> Inputs;
-
-    protected readonly Channel<(float[] Data, int[] Shape)> Outputs;
-
-    private readonly Channel<TaskCompletionSource<(float[], int[])>> _completions;
-    private readonly AsyncLock _writeLock = new();  // Synchronizes writes to Inputs and _completions
-
-    // Reads from Outputs and _completions, completes tasks returned to callers with results
-    private readonly Task _outputCompletionTask;
-
     private readonly InferenceSession _inferenceSession;
 
-    protected ModelRunner(InferenceSession inferenceSession)
-    {
-        Inputs = Channel.CreateUnbounded<(float[], int[])>();
-        Outputs = Channel.CreateUnbounded<(float[], int[])>();
-        _completions = Channel.CreateUnbounded<TaskCompletionSource<(float[], int[])>>();
-        _outputCompletionTask = CompleteOutputs();
-        _inferenceSession = inferenceSession;
-    }
+    protected ModelRunner(InferenceSession inferenceSession) => _inferenceSession = inferenceSession;
 
-    public async Task<(float[], int[])> Run(float[] input, int[] shape)
-    {
-        Debug.Assert(shape.Length > 1);  // At least one dimension
-        var tcs = new TaskCompletionSource<(float[], int[])>();
-        await _writeLock.AcquireAsync(CancellationToken.None);
-        try
-        {
-            await _completions.Writer.WriteAsync(tcs);  // unbounded, will not block
-            await Inputs.Writer.WriteAsync((input, shape));  // bounded, may block
-        }
-        finally
-        {
-            _writeLock.Release();
-        }
+    public abstract Task<(float[] Data, int[] Shape)> Run(float[] batch, int[] shape);
 
-        return await tcs.Task;
-    }
-
-    private async Task CompleteOutputs()
-    {
-        await foreach (var (data, shape) in Outputs.Reader.ReadAllAsync())
-        {
-            if (!_completions.Reader.TryRead(out var tcs))
-            {
-                throw new Exception("This shouldn't happen");
-            }
-
-            tcs.SetResult((data, shape));
-        }
-    }
-
-    // Override for testing
-    protected virtual (float[], int[]) RunInference(float[] batch, int[] shape)
+    protected virtual (float[] Data, int[] Shape) RunInferenceInternal(float[] batch, int[] shape)
     {
         Debug.Assert(shape.Length > 2);  // 1 batch size dimension, at least one other dimension
 
@@ -95,20 +46,13 @@ public abstract class ModelRunner : IAsyncDisposable
         return (output, outputShape);
     }
 
+    protected abstract ValueTask SubclassDisposeAsync();
+
     public async ValueTask DisposeAsync()
     {
-        Inputs.Writer.Complete();
-        await Inputs.Reader.Completion;
-
-        Outputs.Writer.Complete();
-        await Outputs.Reader.Completion;
-
-        await _outputCompletionTask;
-
         await SubclassDisposeAsync();
 
         GC.SuppressFinalize(this);
     }
 
-    protected abstract ValueTask SubclassDisposeAsync();
 }
