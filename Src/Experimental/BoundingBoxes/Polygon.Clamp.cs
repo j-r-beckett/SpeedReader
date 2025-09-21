abc38a0e75d5e0fe6ba4fb662992ca9cdfc116e4
Jimmy Beckett <jimmybeckett1@gmail.com>
2025-09-21 15:13:13 -0700

    Bring Scale and Clamp into Experimental

---
+++ Src/Experimental/BoundingBoxes/Polygon.Clamp.cs
@@ -0,0 +1,18 @@
+// Copyright (c) 2025 j-r-beckett
+// Licensed under the Apache License, Version 2.0
+
+namespace Experimental.BoundingBoxes;
+
+public static partial class PolygonExtensions
+{
+    public static Polygon Clamp(this Polygon polygon, int height, int width)
+    {
+        return new Polygon { Points = polygon.Points.Select(ClampPoint).ToList() };
+
+        Point ClampPoint(Point p) => new()
+        {
+            X = Math.Clamp(p.X, 0, width),
+            Y = Math.Clamp(p.Y, 0, height)
+        };
+    }
+}
