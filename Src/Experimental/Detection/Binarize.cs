f658137c8caa6b4fd395190f0407e6c614b68037
Jimmy Beckett <jimmybeckett1@gmail.com>
2025-09-21 01:08:28 -0700

    Refactor detection and recognition preprocessing

---
+++ Src/Experimental/Detection/Binarize.cs
@@ -0,0 +1,17 @@
+// Copyright (c) 2025 j-r-beckett
+// Licensed under the Apache License, Version 2.0
+
+using System.Diagnostics;
+using System.Numerics.Tensors;
+
+namespace Experimental.Detection;
+
+public static class Binarize
+{
+    public static void BinarizeInPlace(this float[] probabilityMap, float threshold)
+    {
+        Debug.Assert(probabilityMap.Min() >= 0 && probabilityMap.Max() <= 1);
+        Tensor.Subtract(probabilityMap, threshold, probabilityMap);
+        Tensor.Ceiling<float>(probabilityMap, probabilityMap);
+    }
+}
