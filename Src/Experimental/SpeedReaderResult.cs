58eb7f7712973bbb0ddfd0c4ff4ecbaed709b3d0
Jimmy Beckett <jimmybeckett1@gmail.com>
2025-09-27 14:17:21 -0700

    Make E2E tests more comprehensive

--- Src/Experimental/SpeedReaderResult.cs
+++ Src/Experimental/SpeedReaderResult.cs
@@ -14,9 +14,14 @@ public record SpeedReaderResult
     public readonly List<(BoundingBox BBox, string Text, double Confidence)> Results;
     public readonly VizBuilder VizBuilder;
 
+    public List<BoundingBox> Detections;
+    public List<(string Text, double Confidence)> Recognitions;
+
     public SpeedReaderResult(Image<Rgb24> image, List<BoundingBox> detections, List<(string Text, double Confidence)> recognitions, VizBuilder vizBuilder)
     {
         Image = image;
+        Detections = detections;
+        Recognitions = recognitions;
         Results = Enumerable.Range(0, detections.Count)
             .Select(i => (detections[i], recognitions[i].Text, recognitions[i].Confidence))
             .ToList();
