4d5bc6612a51d4e4f8c838b651b777a0896abda2
Jimmy Beckett <jimmybeckett1@gmail.com>
2025-10-02 17:03:16 -0700

    More tuning detection performance

--- Src/Experimental/Algorithms/ImageToTensor.cs
+++ Src/Experimental/Algorithms/ImageToTensor.cs
@@ -14,27 +14,29 @@ public static class PixelsToFloatsExtensions
         var width = shape[1];
         var channels = shape[2];
 
-
-        if (!image.DangerousTryGetSinglePixelMemory(out var imageData))
-        {
-            throw new NonContiguousImageException("Image memory is not contiguous");
-        }
-
         var result = new float[width * height * 3];
 
-        var imageDataSpan = imageData.Span;
-
-        for (int y = 0; y < image.Height; y++)
+        image.ProcessPixelRows(rowAccessor =>
         {
-            for (int x = 0; x < image.Width; x++)
+            for (var y = 0; y < rowAccessor.Height; y++)
             {
-                var destIndex = (y * width + x) * channels;
-                var pixel = x >= width || y >= height
-                    ? new Rgb24(0, 0, 0)
-                    : imageDataSpan[y * image.Width + x];
-                result[destIndex] = pixel.R;
-                result[destIndex + 1] = pixel.G;
-                result[destIndex + 2] = pixel.B;
+                var row = rowAccessor.GetRowSpan(y);
+                for (int x = 0; x < row.Length; x++)
+                {
+                    var destIndex = (y * width + x) * channels;
+                    var pixel = row[x];
+                    result[destIndex] = pixel.R;
+                    result[destIndex + 1] = pixel.G;
+                    result[destIndex + 2] = pixel.B;
+                }
+            }
+        });
+
+        for (int y = image.Height + 1; y < height; y++)
+        {
+            for (int x = image.Width * (int)channels + 1; x < width; x++)
+            {
+                result[y * width * channels + x] = padding;
             }
         }
 
