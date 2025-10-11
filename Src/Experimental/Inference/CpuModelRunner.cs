53845492a59ef363c10d96b77c8cfadaa0bf5899
Jimmy Beckett <jimmybeckett1@gmail.com>
2025-10-11 01:07:19 -0700

    Use ParallelismManager instead of TPL Dataflow

--- Src/Experimental/Inference/CpuModelRunner.cs
+++ Src/Experimental/Inference/CpuModelRunner.cs
@@ -2,51 +2,33 @@
 // Licensed under the Apache License, Version 2.0
 
 using System.Diagnostics;
-using System.Threading.Tasks.Dataflow;
 using Microsoft.ML.OnnxRuntime;
 
 namespace Experimental.Inference;
 
 public class CpuModelRunner : ModelRunner
 {
-    private readonly ActionBlock<(TaskCompletionSource<(float[], int[])>, float[], int[])> _inferenceRunnerBlock;
+    private readonly ParallelismManager<(float[], int[]), (float[], int[])> _parallelismManager;
 
     public CpuModelRunner(InferenceSession inferenceSession, int maxParallelism) : base(inferenceSession)
     {
         ArgumentOutOfRangeException.ThrowIfNegativeOrZero(maxParallelism);
 
-        _inferenceRunnerBlock = new ActionBlock<(TaskCompletionSource<(float[], int[])> Tcs, float[] Data, int[] Shape)>(input =>
+        _parallelismManager = new ParallelismManager<(float[] Data, int[] Shape), (float[], int[])>(input =>
         {
-            try
-            {
-                var (data, shape) = RunInferenceInternal(input.Data, input.Shape);
-                Debug.Assert(shape[0] == 1); // Batch size is always 1 on CPU
-                var unbatchedShape = shape[1..]; // Strip batch size dimension that we added earlier
-                input.Tcs.SetResult((data, unbatchedShape));
-            }
-            catch (InferenceException ex)
-            {
-                input.Tcs.TrySetException(ex);
-            }
-        }, new ExecutionDataflowBlockOptions
-        {
-            MaxDegreeOfParallelism = maxParallelism,
-            BoundedCapacity = maxParallelism
-        });
+            var (data, shape) = RunInferenceInternal(input.Data, input.Shape);
+            Debug.Assert(shape[0] == 1); // Batch size is always 1 on CPU
+            var unbatchedShape = shape[1..]; // Strip batch size dimension that we added earlier
+            return (data, unbatchedShape);
+        }, maxParallelism);
     }
 
     public override Task<Task<(float[] Data, int[] Shape)>> Run(float[] batch, int[] shape)
     {
         Debug.Assert(shape.Length > 1);  // At least one dimension
-        var tcs = new TaskCompletionSource<(float[], int[])>();
         var batchedShape = new[] { 1 }.Concat(shape).ToArray();  // Add a batch size dimension. On CPU we don't batch, so this is just 1
-        return _inferenceRunnerBlock.SendAsync((tcs, batch, batchedShape)).ContinueWith(t => tcs.Task);
-        // return await tcs.Task;
+        return _parallelismManager.Call((batch, batchedShape));
     }
 
-    protected override async ValueTask SubclassDisposeAsync()
-    {
-        _inferenceRunnerBlock.Complete();
-        await _inferenceRunnerBlock.Completion;
-    }
+    protected override ValueTask SubclassDisposeAsync() => ValueTask.CompletedTask;
 }
