bb60aa4655f4be9a7c14049fbcffe1892ddb9bd6
Jimmy Beckett <jimmybeckett1@gmail.com>
2025-10-11 17:41:40 -0700

    Create SharedClock

---
+++ Src/Experimental/Inference/SharedClock.cs
@@ -0,0 +1,13 @@
+// Copyright (c) 2025 j-r-beckett
+// Licensed under the Apache License, Version 2.0
+
+using System.Diagnostics;
+
+namespace Experimental.Inference;
+
+public static class SharedClock
+{
+    private static readonly Stopwatch _clock = Stopwatch.StartNew();
+
+    public static TimeSpan Elapsed => _clock.Elapsed;
+}
