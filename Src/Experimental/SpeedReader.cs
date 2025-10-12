fe3aeac057633ad03454a243286b8e965fdb2248
Jimmy Beckett <jimmybeckett1@gmail.com>
2025-10-11 17:37:45 -0700

    Remove junk

--- Src/Experimental/SpeedReader.cs
+++ Src/Experimental/SpeedReader.cs
@@ -89,9 +89,6 @@ public class SpeedReader
                 var image = await imageTask;
                 var detections = await detector.Detect(image, vizBuilder);
                 var recognitions = await recognizer.Recognize(detections, image, vizBuilder);
-                // var recognitionTasks = detections.Select(d =>
-                // recognizer.Recognize(d.RotatedRectangle, image, vizBuilder));
-                // var recognitions = await Task.WhenAll(recognitionTasks);
                 Debug.Assert(detections.Count == recognitions.Count);
                 return new SpeedReaderResult(image, detections, recognitions.ToList(), vizBuilder);
             }
