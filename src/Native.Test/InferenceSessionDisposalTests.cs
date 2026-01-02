52d6f5e9770f4fabb6443097312eb72b61aa27d1
Jimmy Beckett <jimmybeckett1+github@gmail.com>
2026-01-01 22:07:50 -0800

    Test InferenceSession.Dispose

---
+++ src/Native.Test/InferenceSessionDisposalTests.cs
@@ -0,0 +1,35 @@
+// Copyright (c) 2025 j-r-beckett
+// Licensed under the Apache License, Version 2.0
+
+using SpeedReader.Resources.Weights;
+
+namespace SpeedReader.Native.Test;
+
+public class InferenceSessionDisposalTests
+{
+    [Fact]
+    public void Dispose_Works()
+    {
+        var model = EmbeddedWeights.Dbnet_Int8.Bytes;
+        var session = new InferenceSession(model);
+        session.Dispose();
+    }
+
+    [Fact]
+    public void Dispose_Twice_Works()
+    {
+        var model = EmbeddedWeights.Dbnet_Int8.Bytes;
+        var session = new InferenceSession(model);
+        session.Dispose();
+        session.Dispose();
+    }
+
+    [Fact]
+    public void Run_AfterDisposal_ThrowsObjectDisposedException()
+    {
+        var model = EmbeddedWeights.Dbnet_Int8.Bytes;
+        var session = new InferenceSession(model);
+        session.Dispose();
+        Assert.Throws<ObjectDisposedException>(() => session.Run(null!, null!));
+    }
+}
