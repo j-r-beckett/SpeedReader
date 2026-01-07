b65c4cc779520f0ff8b25f418460cf0bcd08af87
Jimmy Beckett <jimmybeckett1+github@gmail.com>
2026-01-07 12:47:02 -0800

    Refine thread pool

--- src/Ocr/InferenceEngine/Engines/AffinitizedThreadPool.cs
+++ src/Ocr/InferenceEngine/Engines/AffinitizedThreadPool.cs
@@ -11,43 +11,54 @@ public class AffinitizedThreadPool : IDisposable
 {
     private readonly ConcurrentQueue<Action> _dbnetActions = new();
     private readonly ConcurrentQueue<Action> _svtrActions = new();
-    private readonly ConcurrentQueue<ManagedThread> _dbnetThreads = new();
-    private readonly ConcurrentQueue<ManagedThread> _svtrThreads = new();
+    private readonly Queue<ManagedThread> _reservedPThreads = new();
+    private readonly Queue<ManagedThread> _unreservedPThreads = new();
+    private readonly Queue<ManagedThread> _eThreads = new();
 
     private readonly Thread _dispatcherThread;
     private readonly CancellationTokenSource _dispatcherCts = new();
     private readonly SemaphoreSlim _semaphore = new(0);
 
+    private readonly Lock _threadsLock = new();
+
     private readonly List<ManagedThread> _allThreads = [];
+    private readonly Dictionary<ManagedThread, Queue<ManagedThread>> _threadTargetQueues = new();
 
-    public AffinitizedThreadPool(Span<int> dbnetCores, Span<int> svtrCores)
+    public AffinitizedThreadPool(Span<int> pCores, Span<int> eCores)
     {
-        if (dbnetCores.IsEmpty)
-            throw new ArgumentException("No dbnet cores specified", nameof(dbnetCores));
+        if (pCores.IsEmpty && eCores.IsEmpty)
+            throw new ArgumentException("At least one core must be specified");
 
-        if (svtrCores.IsEmpty)
-            throw new ArgumentException("No svtr cores specified", nameof(svtrCores));
+        if (pCores.IsEmpty)
+        {
+            pCores = eCores;
+            eCores = [];
+        }
 
-        foreach (var core in dbnetCores)
-            AddThread(_dbnetThreads, core);
+        foreach (var core in pCores)
+            AddThread(_reservedPThreads, core);
 
-        foreach (var core in svtrCores)
-            AddThread(_svtrThreads, core);
+        foreach (var core in eCores)
+            AddThread(_eThreads, core);
 
         _dispatcherThread = new Thread(DispatcherThreadProc) { IsBackground = true };
         _dispatcherThread.Start();
 
         return;
 
-        void AddThread(ConcurrentQueue<ManagedThread> threadQueue, int core)
+        void AddThread(Queue<ManagedThread> threadQueue, int core)
         {
-            var thread = new ManagedThread(thread =>
-            {
-                threadQueue.Enqueue(thread);
-                _semaphore.Release();
-            }, core);
+            var thread = new ManagedThread(RequeueThreadCallback, core);
+            _threadTargetQueues[thread] = threadQueue;
             threadQueue.Enqueue(thread);
             _allThreads.Add(thread);
+        }
+
+        void RequeueThreadCallback(ManagedThread thread)
+        {
+            lock (_threadsLock)
+                _threadTargetQueues[thread].Enqueue(thread);
+            _semaphore.Release();
         }
     }
 
@@ -57,35 +68,66 @@ public class AffinitizedThreadPool : IDisposable
         {
             while (true)
             {
-                // Preferentially:
-                //   SVTR job -> SVTR core
-                //   DBNet job -> DBNet core
-                //   SVTR job -> DBNet core
-                // Where SVTR cores are E-cores and unused P-cores, while DBNet cores are a subset of P-cores
+                // Goal: maximize combined DBNet and SVTR throughput while keeping DBNet latency manageable.
+                //       Backpressure is NOT in scope (that's managed by an external system).
+
+                // Basic facts:
+                //   - DBNet is DRAM bandwidth bound
+                //   - SVTR is CPU bound, but it still consumes some DRAM bandwidth
+                //   - SVTR jobs are downstream of DBNet jobs--a successfully completed DBNet job spawns SVTR jobs
+                //   - DBNet is slow, ~125 ms. SVTR is fast, ~10 ms
+                //   - Overall system latency is primary a function of DBNet latency
+                //   - Compared to P-cores, E-cores have both less compute and less ability to drive memory. But the gap
+                //     between P-cores and E-cores on compute is typically smaller than the gap on ability to drive memory
+
+                // Deductions:
+                //   - If we run DBNet on an E-core, lack of memory bandwidth will substantially increase latency. This
+                //     will kill overall system latency. Additionally, on a typical system P-cores are sufficient to
+                //     saturate memory bandwidth. So don't run DBNet on E-cores!
+                //   - Running SVTR jobs on P-cores is fine, as long as it doesn't slow down DBNet jobs by stealing
+                //     needed memory bandwidth
+                //   - DBNet is memory bandwidth limited, so we can't just throw cores at it. But we can throw cores at
+                //     SVTR! This suggests the following architecture: carefully manage the amount of cores devoted
+                //     to DBNet, and give SVTR the rest of the cores on the system
+                //   - SVTR jobs do consume some memory bandwidth. We want to avoid our SVTR jobs stealing bandwidth
+                //     from DBNet jobs. So we only run a SVTR job on a P-core if we're keeping up with our DBNet jobs
+
+                // Algorithm:
+                //   Preferentially:
+                //     - Run a [SVTR] job on an [E-core]
+                //     - Run a [DBNet] job on a [reserved P-core]
+                //     - Run a [SVTR] job on an [unreserved P-core]
+                //     - Run a [SVTR] job on a [reserved P-core]
+
 
                 _semaphore.Wait(_dispatcherCts.Token);
-                if (!_svtrActions.IsEmpty && !_svtrThreads.IsEmpty)
-                    Dispatch(_svtrActions, _svtrThreads);
-                else if (!_dbnetActions.IsEmpty && !_dbnetThreads.IsEmpty)
-                    Dispatch(_dbnetActions, _dbnetThreads);
-                else if (!_svtrActions.IsEmpty && !_dbnetThreads.IsEmpty)
-                    Dispatch(_svtrActions, _dbnetThreads);
+                lock (_threadsLock)
+                {
+                    if (!_svtrActions.IsEmpty && _eThreads.Count > 0)
+                        Dispatch(_svtrActions, _eThreads);
+                    else if (!_dbnetActions.IsEmpty && _reservedPThreads.Count > 0)
+                        Dispatch(_dbnetActions, _reservedPThreads);
+                    else if (!_svtrActions.IsEmpty && _unreservedPThreads.Count > 0)
+                        Dispatch(_svtrActions, _unreservedPThreads);
+                    else if (!_svtrActions.IsEmpty && _reservedPThreads.Count > 0)
+                        Dispatch(_svtrActions, _reservedPThreads);
+                }
             }
         }
         catch (OperationCanceledException) { }
 
         return;
 
-        static void Dispatch(ConcurrentQueue<Action> actions, ConcurrentQueue<ManagedThread> threads)
+        static void Dispatch(ConcurrentQueue<Action> actions, Queue<ManagedThread> threads)
         {
-            threads.TryDequeue(out var thread);
-            Debug.Assert(thread != null);
+            var thread = threads.Dequeue();
             actions.TryDequeue(out var action);
             Debug.Assert(action != null);
             thread.Run(action);
         }
     }
 
+    // Cannot be called concurrent with Dispose
     public Task<T> Run<T>(Func<T> func, Model model)
     {
         ObjectDisposedException.ThrowIf(_dispatcherCts.IsCancellationRequested, this);
@@ -108,8 +150,37 @@ public class AffinitizedThreadPool : IDisposable
                 tcs.SetException(ex);
             }
         });
+
         _semaphore.Release();
+
         return tcs.Task;
+    }
+
+    public void ReservePCore()
+    {
+        lock (_threadsLock)
+        {
+            if (_unreservedPThreads.Count == 0)
+                throw new InvalidOperationException("No unreserved P-core available");
+            var thread = _unreservedPThreads.Dequeue();
+            _threadTargetQueues[thread] = _reservedPThreads;
+            _reservedPThreads.Enqueue(thread);
+        }
+        _semaphore.Release();
+    }
+
+    public void ReleasePCore()
+    {
+        lock (_threadsLock)
+        {
+            Debug.Assert(_reservedPThreads.Count != 0);
+            if (_reservedPThreads.Count == 1)
+                throw new InvalidOperationException("Cannot release last reserved P-core");
+            var thread = _reservedPThreads.Dequeue();
+            _threadTargetQueues[thread] = _unreservedPThreads;
+            _unreservedPThreads.Enqueue(thread);
+        }
+        _semaphore.Release();
     }
 
     public int Size => _allThreads.Count;
