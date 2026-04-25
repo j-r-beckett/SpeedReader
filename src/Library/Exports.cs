4413026b8766f603e17294fbf460480b50896973
Jimmy Beckett <jimmybeckett1+github@gmail.com>
2026-04-24 20:33:50 -0700

    Expand ffi boilerplate

--- src/Library/Exports.cs
+++ src/Library/Exports.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0
 
 using System.Runtime.InteropServices;
+using System.Text;
 
 namespace SpeedReader.Library;
 
@@ -14,24 +15,100 @@ public static unsafe class Exports
     private const int ErrorBufSize = 256;
 
     [UnmanagedCallersOnly(EntryPoint = "speedreader_create")]
-    public static int Create(nint* instance, byte* error) => Ok;
+    public static int Create(nint* instance, byte* error)
+    {
+        try
+        {
+            var inst = new Instance();
+            var gcHandle = GCHandle.Alloc(inst);
+            *instance = GCHandle.ToIntPtr(gcHandle);
+            return Ok;
+        }
+        catch (Exception ex)
+        {
+            WriteError(error, ex.Message);
+            return Error;
+        }
+    }
 
     [UnmanagedCallersOnly(EntryPoint = "speedreader_destroy")]
     public static void Destroy(nint instance)
     {
+        try
+        {
+            var gcHandle = GCHandle.FromIntPtr(instance);
+            gcHandle.Free();
+        }
+        catch
+        {
+            // Best-effort. Nothing to report to.
+        }
     }
 
     [UnmanagedCallersOnly(EntryPoint = "speedreader_submit")]
-    public static int Submit(nint instance, byte* imageData, nuint imageLen, long* handle, byte* error) => Ok;
+    public static int Submit(nint instance, byte* imageData, nuint imageLen, long* handle, byte* error)
+    {
+        try
+        {
+            return Ok;
+        }
+        catch (Exception ex)
+        {
+            WriteError(error, ex.Message);
+            return Error;
+        }
+    }
 
     [UnmanagedCallersOnly(EntryPoint = "speedreader_await")]
-    public static int Await(nint instance, long handle, int timeoutMs, byte** resultJson, nuint* resultLen, byte* error) => Ok;
+    public static int Await(nint instance, long handle, int timeoutMs, byte** resultJson, nuint* resultLen, byte* error)
+    {
+        try
+        {
+            return Ok;
+        }
+        catch (Exception ex)
+        {
+            WriteError(error, ex.Message);
+            return Error;
+        }
+    }
 
     [UnmanagedCallersOnly(EntryPoint = "speedreader_cancel")]
-    public static int Cancel(nint instance, long handle) => Ok;
+    public static int Cancel(nint instance, long handle)
+    {
+        try
+        {
+            return Ok;
+        }
+        catch
+        {
+            return Error;
+        }
+    }
 
     [UnmanagedCallersOnly(EntryPoint = "speedreader_free_result")]
     public static void FreeResult(byte* resultJson)
     {
+        try
+        {
+            if (resultJson != null)
+                NativeMemory.Free(resultJson);
+        }
+        catch
+        {
+            // Best-effort.
+        }
+    }
+
+    private static Instance GetInstance(nint instance) => (Instance)GCHandle.FromIntPtr(instance).Target!;
+
+    private static void WriteError(byte* error, string message)
+    {
+        if (error == null)
+            return;
+
+        var maxBytes = ErrorBufSize - 1;
+        var written = Encoding.UTF8.GetBytes(message.AsSpan(), new Span<byte>(error, maxBytes));
+        error[written] = 0;
     }
 }
---
+++ src/Library/Instance.cs
@@ -0,0 +1,8 @@
+// Copyright (c) 2025 j-r-beckett
+// Licensed under the Apache License, Version 2.0
+
+namespace SpeedReader.Library;
+
+internal class Instance
+{
+}
